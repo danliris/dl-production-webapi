2cffbc165566f6dec6bd1db8c601928bcf419a30
jackyrusly <jacky.rusly@CARTIER.moonlay.jakarta>
2017-12-07 20:22:33 +0700

    Order Status Detail

--- src/routers/v1/sales/reports/order-status-report-router.js
+++ src/routers/v1/sales/reports/order-status-report-router.js
@@ -54,6 +54,42 @@ function getRouter() {
                 response.send(statusCode, error);
             });
     });
+
+    router.get("/:year/:month/:orderType", passport, function (request, response, next) {
+        var user = request.user;
+        var query = request.query;
+        query.order = Object.assign({}, defaultOrder, query.order);
+
+        var productionOrderManager = {};
+        getManager(user)
+            .then((manager) => {
+                productionOrderManager = manager;
+                return productionOrderManager.getOrderStatusDetailReport(request.params);
+            })
+            .then((data) => {
+                var result = resultFormatter.ok(apiVersion, 200, data);
+                return Promise.resolve(result);
+            })
+            .then((result) => {
+                if ((request.headers.accept || '').toString().indexOf("application/xls") < 0) {
+                    response.send(result.statusCode, result);
+                }
+                else {
+                    productionOrderManager.getOrderStatusDetailXls(result, request.params)
+                        .then((xls) => {
+                            response.xls(xls.name, xls.data, xls.options)
+                        });
+                }
+            })
+            .catch((e) => {
+                var statusCode = 500;
+                if (e.name === "ValidationError")
+                    statusCode = 400;
+                var error = resultFormatter.fail(apiVersion, statusCode, e);
+                response.send(statusCode, error);
+            });
+    });
+
     return router;
 }
 
